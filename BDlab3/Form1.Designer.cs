70f86122ec35e16712a088fe2213faf1b30327d8
kirich59 <kirillbazhenov45@gmail.com>
2018-10-16 20:05:45 +0500

    Добавьте файлы проекта.

---
+++ BDlab3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BDlab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgrd = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbOut = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgrd
+            // 
+            this.dtgrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrd.Location = new System.Drawing.Point(12, 75);
+            this.dtgrd.Name = "dtgrd";
+            this.dtgrd.RowTemplate.Height = 24;
+            this.dtgrd.Size = new System.Drawing.Size(746, 617);
+            this.dtgrd.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(745, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите команду";
+            // 
+            // rtbOut
+            // 
+            this.rtbOut.Location = new System.Drawing.Point(765, 75);
+            this.rtbOut.Name = "rtbOut";
+            this.rtbOut.ReadOnly = true;
+            this.rtbOut.Size = new System.Drawing.Size(173, 617);
+            this.rtbOut.TabIndex = 3;
+            this.rtbOut.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 704);
+            this.Controls.Add(this.rtbOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dtgrd);
+            this.Name = "Form1";
+            this.Text = "Database Commander";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgrd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbOut;
+    }
+}
+
---
+++ BDlab3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BDlab3
+{
+    public partial class Form1 : Form
+    {
+        public static MySqlConnection
+                 GetDBConnection(string host, int port, string database, string username, string password)
+        {
+            // Connection String.
+            String connString = "Server=" + host + ";Database=" + database
+                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            return conn;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                MySqlConnection conn = GetDBConnection("127.0.0.1", 3306, "bd_hse", "root", "5962kirill");
+                try
+                {
+                    MySqlCommand mycom = new MySqlCommand(textBox1.Text, conn);
+                    conn.Open();
+                    MySqlDataAdapter adap = new MySqlDataAdapter(mycom);
+                    DataSet ds = new DataSet();
+                    adap.Fill(ds);
+                    dtgrd.DataSource = ds.Tables[0];
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    rtbOut.Text += "Произошла ошибка : ";
+                    rtbOut.Text += ex.Message;
+                }
+            }
+        }
+    }
+}
